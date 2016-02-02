ab89079064e0e2abe358ef3fd2cfd069c0670f7c
Leah Fisher <lfisher047@gmail.com>
2016-02-02 14:10:23 -0500

    Changing log to include comment, not port.

--- manifests/firewall/proto_dport_source.pp
+++ manifests/firewall/proto_dport_source.pp
@@ -38,7 +38,7 @@ define rhel::firewall::proto_dport_source (
     proto    => $proto,
     source   => $source,
     provider => $provider,
-    log_prefix => "$chain $action $dport: ",
+    log_prefix => "iptables $chain $action $prefix: ",
     log_level  => '7'
   }
   
